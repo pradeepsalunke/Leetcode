a6a240fa1503fd30ea58b6e441f3caee9e7162ae
Pradeep Salunke <pradeep.salunke@yahoo.com>
2020-07-03 20:35:42 -0400

    New hashing implement

--- HashTable.py
+++ HashTable.py
@@ -1,47 +1,105 @@
 class HashTable:
-
+    #constructor
     def __init__(self):
-        self.size = 256
-        self.hashmap = [[] for _ in range(0, self.size)]
-        # print(self.hashmap)
+        #max size of dict in python
+        self.size = 2
+        #creating an empty list
+        self.map = [None] * self.size #[[],[],[]]
+        print(self.map)
 
-    def hash_func(self, key):
-        hashed_key = hash(key) % self.size
-        return hashed_key
+    #hash funcation
+    def hash_func(self,key):
+        hashed_key=0
+        #creating an hash using ASCII value by adding ascii values of each char
+        # and by dividing by total size to distribute equally among map -list
 
-    def set(self, key, value):
-        hash_key = self.hash_func(key)
-        key_exists = False
-        slot = self.hashmap[hash_key]
-        for i, kv in enumerate(slot):
-            k, v = kv
-            if key == k:
-                key_exists = True
-                break
+        for char in key:
+            hashed_key += ord(char)
+        return hashed_key%self.size
 
-        if key_exists:
-            slot[i] = ((key, value))
+    #add or set
+    def add(self,key,value):
+        #get memory address where we wanna store our key, value this address is a hashed one
+        hashed_key=self.hash_func(key)
+        #map [[slot1],[slot2],[slot3]]
+        #slot=[key,value]
+        #row==slot==bucket
+        slot=[key,value]
+        #if the address is empty place our slot in that address
+        if self.map[hashed_key] is None:
+            self.map[hashed_key] = list([slot])
+            return True
+        #if data exist on that address then
+        #1.check if key are same that is operation of update
+        #2.if keys are different then append to the list(chain, linkedList)
         else:
-            slot.append((key, value))
+            for pair in self.map[hashed_key]:
+                #[key,value]
+                #[  0 , 1  ]
+                if pair[0]==key:
+                    #update
+                    pair[1] = value
+                    return True
+            #else append
+            self.map[hashed_key].append(slot)
+            return True
+    #lookup operation
+    def get(self,key):
+        #get hash of key  or get location of that key
+        hashed_key=self.hash_func(key)
+        #if data at location is not none
+        if self.map[hashed_key] is not None:
+            #get the key value on that address
+            for pair in self.map[hashed_key]:
+                #if key matchs return value
+                if pair[0]==key:
+                    return pair[1]
+        return None
 
-    def get(self, key):
-        hash_key = self.hash_func(key)
-        slot = self.hashmap[hash_key]
-        for kv in slot:
-            k, v = kv
-            if key == k:
-                return v
-            else:
-                raise KeyError('Key does not exist.')
+    def delete(self,key):
+        hashed_key=self.hash_func(key)
+        if self.map[hashed_key] is None:
+            return False
+        #we use range because we need index in the list to pop
+        for i in range(0,len(self.map[hashed_key])):
+            #
+            if self.map[hashed_key][i][0]==key:
+                self.map[hashed_key].pop(i)
+                return True
+        return False
+    def print(self):
+        print('---PHONEBOOK----')
+        print(self.map)
+        for item in self.map:
+            if item is not None:
+                print(str(item))
 
-    def __setitem__(self, key, value):
-        return self.set(key, value)
+h = HashTable()
+h.add('Bob', '567-8888')
+h.add('Ming', '293-6753')
+h.add('Ming', '333-8233')
+h.add('Ankit', '293-8625')
+h.add('Aditya', '852-6551')
+h.add('Alicia', '632-4123')
+h.add('Mike', '567-2188')
+h.print()
+print(h.get('Mike'))
+h.print()
+h.delete('Bob')
+h.print()
 
-    def __getitem__(self, key):
-        return self.get(key)
 
 
 
+
+
+
+
+
+
+
+
+'''
 H = HashTable()
 H.set('key1','value1')
 H.set('key2','value2')
@@ -55,5 +113,6 @@ H['NEWWWWWWWWW'] = 'newwwwwwwww'
 print(H['key1'])
 print(H[10])
 print(H[20])
-
+print(H[30])
 print(H.hashmap)
+'''
